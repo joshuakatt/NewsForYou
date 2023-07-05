caf8f5be3b1eb55ddbf1971801a099d4ec2d694d
Joshua Kattapuram <70146035+joshuakatt@users.noreply.github.com>
2023-07-05 01:25:07 -0230

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# News_For_You
+ A markdown blog leveraging NodeJS, Express, and MongoDB, and future AI functionality along with to generate news articles
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const articleRouter = require('./routes/articles')
+
+mongoose.connect('mongodb://localhost/BLOG_1.0', {useNewUrlParser: true}, {useUnifiedTopology: true})
+
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({extended: false}))
+
+
+
+app.get('/', (req, res) =>
+        
+    {
+        const articles = [
+        {
+            title: "Test Article",
+            createdAt: new Date(),
+            description: 'Test Description'
+        },
+        {
+            title: "Test Article 2",
+            createdAt: new Date(),
+            description: 'Test Description 2'
+        }
+    ]
+        res.render('articles/index', {articles: articles })
+    })
+
+app.use('/articles', articleRouter)
+app.listen(8000)
